3ae2a27d3116af78cae6c2c1b6fe4059b4da5430
Lina Wellerstrand <lina.wellerstrand@gmail.com>
2015-10-22 13:14:46 +0200

    First commit..

---
+++ RadioSystemTest/TestRadioFacade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RadioSystemTest
+{
+    [TestClass]
+    public class TestRadioFacade
+    {
+        [TestMethod]
+        public void Test_That_Facade_Can_Be_Created()
+        {
+            RadioFacade facade = new RadioFacade();
+
+            Assert.IsNotNull(facade);
+        }
+
+        //För att testet ska gå igenom nu, byt ut NotImplementedException -> NullReferenceException
+
+        //[TestMethod]
+        //[ExpectedException(typeof(NotImplementedException))]
+        //public void Test_That_MixMegapol_Generates_Exception()
+        //{
+        //    RadioFacade facade = new RadioFacade();
+
+        //    facade.PlayMusic("MixMegapol");
+        //}
+
+        [TestMethod]
+        public void Test_That_RixFM_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+
+            facade.PlayMusic("RixFM");
+
+            Assert.IsTrue(facade.IsPlaying);
+        }
+
+        [TestMethod]
+        public void Test_That_P3_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+
+            facade.PlayMusic("P3");
+
+            Assert.IsTrue(facade.IsPlaying);
+        }
+    }
+}
